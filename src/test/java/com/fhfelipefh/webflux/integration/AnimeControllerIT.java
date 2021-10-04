8b2d88bd1ad2abee0189633a7d6e42fc0ae6b00f
Felipe Honorio Moraes <felipehonorio55@gmail.com>
2021-10-04 19:07:36 -0300

    Create AnimeControllerIT.java

---
+++ src/test/java/com/fhfelipefh/webflux/integration/AnimeControllerIT.java
@@ -0,0 +1,89 @@
+package com.fhfelipefh.webflux.integration;
+
+import com.fhfelipefh.webflux.model.Anime;
+import com.fhfelipefh.webflux.repository.AnimeRepository;
+import com.fhfelipefh.webflux.service.AnimeService;
+import com.fhfelipefh.webflux.util.AnimeCreator;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.blockhound.BlockHound;
+import reactor.blockhound.BlockingOperationError;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
+
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+
+@ExtendWith(SpringExtension.class)
+@WebFluxTest
+@Import(AnimeService.class)
+public class AnimeControllerIT {
+
+    @MockBean
+    private AnimeRepository animeRepositoryMock;
+    @Autowired
+    private WebTestClient testClient;
+
+    private final Anime anime = AnimeCreator.createValidAnime();
+
+    @BeforeAll
+    public static void blockHoundSetup() {
+        BlockHound.install();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        BDDMockito.when(animeRepositoryMock.findAll())
+                .thenReturn(Flux.just(anime));
+    }
+
+    @Test
+    public void blockHoundWorks() {
+        try {
+            FutureTask<?> task = new FutureTask<>(() -> {
+                Thread.sleep(0); //NOSONAR
+                return "";
+            });
+            Schedulers.parallel().schedule(task);
+
+            task.get(10, TimeUnit.SECONDS);
+            Assertions.fail("should fail");
+        } catch (Exception e) {
+            Assertions.assertTrue(e.getCause() instanceof BlockingOperationError);
+        }
+    }
+
+    @Test
+    @DisplayName("listAll returns a flux of anime")
+    public void listAll_ReturnFluxOfAnime_WhenSuccessful() {
+        testClient
+                .get()
+                .uri("/animes")
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody()
+                .jsonPath("$.[0].id").isEqualTo(anime.getId())
+                .jsonPath("$.[0].name").isEqualTo(anime.getName());
+    }
+
+    @Test
+    @DisplayName("listAll returns a flux of anime")
+    public void listAll_Flavor2_ReturnFluxOfAnime_WhenSuccessful() {
+        testClient
+                .get()
+                .uri("/animes")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Anime.class)
+                .hasSize(1)
+                .contains(anime);
+    }
+}
